edc27830376b3c8b4a6127414d8829df0a27b220
David Squire <squiredd@gmail.com>
2020-02-03 20:50:25 -0800

    add compression to byte transfer

--- Source/Rice.Core/Transport/TransportableModuleIo.cs
+++ Source/Rice.Core/Transport/TransportableModuleIo.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Rice.Core.Abstractions.Transport;
 
@@ -23,16 +25,34 @@ namespace Rice.Core.Transport
             var nameToUse = string.IsNullOrWhiteSpace(assemblyName)
                 ? Path.GetFileNameWithoutExtension(dllFileInfo.Name)
                 : assemblyName;
-            
-            using (var fileStream = dllFileInfo.OpenRead())
+
+            var buffer = await GetFileBytes(dllFileInfo).ConfigureAwait(false);
+            var compressed = await CompressBytes(buffer);
+
+            var d = await GetDependencies(dependencies).ConfigureAwait(false);
+            var t = new TransportableModule(nameToUse, compressed, d);
+                
+            return t;
+        }
+
+        private async Task<byte[]> CompressBytes(byte[] buffer, CancellationToken cancellationToken = default)
+        {
+            var outputStream = new MemoryStream();
+            using (var gzipStream = new GZipStream(outputStream, CompressionMode.Compress))
             {
-                byte[] buffer = new byte[dllFileInfo.Length];
-                var count = await fileStream
-                    .ReadAsync(buffer, 0, (int) dllFileInfo.Length)
-                    .ConfigureAwait(false);
-                var d = await GetDependencies(dependencies);
-                return new TransportableModule(nameToUse, buffer, d);
+                await new MemoryStream(buffer).CopyToAsync(gzipStream, cancellationToken).ConfigureAwait(false);
             }
+            return outputStream.ToArray();
+        }
+
+        private async Task<byte[]> DeCompressBytes(byte[] buffer, CancellationToken cancellationToken = default)
+        {
+            var outputStream = new MemoryStream();
+            using (var gzipStream = new GZipStream(new MemoryStream(buffer), CompressionMode.Decompress))
+            {
+                await gzipStream.CopyToAsync(outputStream, cancellationToken).ConfigureAwait(false);
+            }
+            return outputStream.ToArray();
         }
 
         // need to make this a separate factory class
@@ -47,15 +67,20 @@ namespace Rice.Core.Transport
             var nameToUse = string.IsNullOrWhiteSpace(assemblyName)
                 ? Path.GetFileNameWithoutExtension(dllFileInfo.Name)
                 : assemblyName;
-            
-            using (var fileStream = dllFileInfo.OpenRead())
-            {
-                byte[] buffer = new byte[dllFileInfo.Length];
-                var count = await fileStream
-                    .ReadAsync(buffer, 0, (int) dllFileInfo.Length)
-                    .ConfigureAwait(false);
-                return new TransportableDependency(nameToUse, buffer);
-            }
+
+            var buffer = await GetFileBytes(dllFileInfo).ConfigureAwait(false);
+            var compressed = await CompressBytes(buffer);
+            return new TransportableDependency(nameToUse, compressed);
+        }
+
+        private static async Task<byte[]> GetFileBytes(FileInfo dllFileInfo)
+        {
+            await using var fileStream = dllFileInfo.OpenRead();
+            byte[] buffer = new byte[dllFileInfo.Length];
+            var count = await fileStream
+                .ReadAsync(buffer, 0, (int) dllFileInfo.Length)
+                .ConfigureAwait(false);
+            return buffer;
         }
 
         private async Task<IEnumerable<ITransportableDependency>> GetDependencies(IEnumerable<(string fullPath, string assemblyName)> dependencies)
@@ -63,7 +88,7 @@ namespace Rice.Core.Transport
             var tuple = dependencies?.ToArray();
             if (tuple == null || !tuple.Any()) return null;
             var transportableDependencyTasks = tuple.Select(d => CreateDependency(d.fullPath, d.assemblyName));
-            var y = await Task.WhenAll(transportableDependencyTasks);
+            var y = await Task.WhenAll(transportableDependencyTasks).ConfigureAwait(false);
             return y;
         }
 
@@ -71,7 +96,8 @@ namespace Rice.Core.Transport
         {
             using (FileStream fs = new FileStream(fullPath, FileMode.Create, FileAccess.Write, FileShare.Read))
             {
-                await fs.WriteAsync(module.Bytes, 0, module.Bytes.Length);
+                var bytes = await DeCompressBytes(module.Bytes).ConfigureAwait(false);
+                await fs.WriteAsync(bytes, 0, bytes.Length).ConfigureAwait(false);
             }
 
             var dependencies = module.Dependencies?.ToArray() ?? new ITransportableDependency[0];
@@ -83,7 +109,8 @@ namespace Rice.Core.Transport
                     var path = Path.Combine(directory.FullName, $"{dependency.AssemblyName}.dll");
                     using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read))
                     {
-                        await fs.WriteAsync(dependency.Bytes, 0, dependency.Bytes.Length);
+                        var bytes = await DeCompressBytes(dependency.Bytes).ConfigureAwait(false);
+                        await fs.WriteAsync(bytes, 0, bytes.Length).ConfigureAwait(false);
                     }
                 }
             }
